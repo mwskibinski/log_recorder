1797194b6c5af284da7f43506115ea7a3a24f4d0
mwskibinski <mwskibinski@gmail.com>
2023-11-11 14:56:22 +0100

    Implement hex dump a file function

--- bytes_into_file.py
+++ bytes_into_file.py
@@ -1,7 +1,27 @@
+def hex_dump_a_file(path_to_file, max_row_len=50):
+	file = open(path_to_file, "rb")
+	max_bytes_read = (max_row_len - 11) // 3
+	addr = 0
+	while True:
+		read_bytes = file.read(max_bytes_read)
+		print("{:04X} {:04X}: ".format((addr & 0xFFFF_0000) >> 16, addr & 0x0000_FFFF), end='')
+		addr += len(read_bytes)
+		print_bytes_as_hex_dump(read_bytes)
+		if len(read_bytes) != max_bytes_read:
+			break
+	file.close()
+
 def print_bytes_as_hex_dump(byte_obj):
+	print(bytes_into_hex_dump(byte_obj))
+
+def bytes_into_hex_dump(byte_obj):
 	hex_list = ["{:02X}".format(b) for b in byte_obj]
 	txt = " ".join(hex_list)
-	print(txt)
+	return txt
+
+hex_dump_a_file('bytes_dummy.bin')
+
+exit()
 
 file = open("bytes.bin", "wb")
 rx_line = "[asd][123123]*~`trc00000: 0123456789\r\n".encode()
